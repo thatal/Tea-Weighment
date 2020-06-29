5885d67d76d09bc4a1dacb26fb1eb21f5444c1fc
Sunil Thatal <sunilthatal007@gmail.com>
2020-06-29 20:26:25 +0530

    quick fix 2

--- resources/views/factory/common/summary-vendor-offers.blade.php
+++ resources/views/factory/common/summary-vendor-offers.blade.php
@@ -14,41 +14,42 @@
         </tr>
     </thead>
     <tbody>
-        @forelse($summary_reports["records"] as $key => $summary)
-            <tr>
-                <th colspan="10">
-                    {{$summary["vendor"]["name"] ?? "N/A"}}
-                </th>
-            </tr>
-            @foreach ($summary["data"] as $row)
+        @if(isset($summary_reports["records"]))
+            @forelse($summary_reports["records"] as $key => $summary)
                 <tr>
-                    <td>{{$row->date}}</td>
-                    <td>{{$row->confirmation_code}}</td>
-                    <td>{{$row->vehicle_number}}</td>
-                    <td class="text-right">{{$row->gross}}</td>
-                    <td class="text-right">{{$row->tare}}</td>
-                    <td class="text-right">{{$row->deduction}}</td>
-                    <td class="text-right">{{$row->sum_weight}}</td>
-                    <td class="text-right">{{$row->fine_leaf}}</td>
-                    <td class="text-right">{{$row->rate}}</td>
-                    <td class="text-right">{{$row->amount}}</td>
+                    <th colspan="10">
+                        {{$summary["vendor"]["name"] ?? "N/A"}}
+                    </th>
                 </tr>
-            @endforeach
+                @foreach ($summary["data"] as $row)
+                    <tr>
+                        <td>{{$row->date}}</td>
+                        <td>{{$row->confirmation_code}}</td>
+                        <td>{{$row->vehicle_number}}</td>
+                        <td class="text-right">{{$row->gross}}</td>
+                        <td class="text-right">{{$row->tare}}</td>
+                        <td class="text-right">{{$row->deduction}}</td>
+                        <td class="text-right">{{$row->sum_weight}}</td>
+                        <td class="text-right">{{$row->fine_leaf}}</td>
+                        <td class="text-right">{{$row->rate}}</td>
+                        <td class="text-right">{{$row->amount}}</td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <th colspan="3">Sub Total</th>
+                    <td class="text-right">{{$summary["sub_total_gross"]}}</td>
+                    <td class="text-right">{{$summary["sub_total_tare"]}}</td>
+                    <td class="text-right">{{$summary["sub_total_deduction"]}}</td>
+                    <td class="text-right">{{$summary["sub_total_net_weight"]}}</td>
+                    <td class="text-right">{{$summary["sub_total_fine_leaf"]}}</td>
+                    <td class="text-right">{{$summary["sub_total_rate"]}}</td>
+                    <td class="text-right">{{$summary["sub_total_amount"]}}</td>
+                </tr>
+            @empty
             <tr>
-                <th colspan="3">Sub Total</th>
-                <td class="text-right">{{$summary["sub_total_gross"]}}</td>
-                <td class="text-right">{{$summary["sub_total_tare"]}}</td>
-                <td class="text-right">{{$summary["sub_total_deduction"]}}</td>
-                <td class="text-right">{{$summary["sub_total_net_weight"]}}</td>
-                <td class="text-right">{{$summary["sub_total_fine_leaf"]}}</td>
-                <td class="text-right">{{$summary["sub_total_rate"]}}</td>
-                <td class="text-right">{{$summary["sub_total_amount"]}}</td>
+                <td class="text-danger text-center" colspan="7">No records found.</td>
             </tr>
-        @empty
-        <tr>
-            <td class="text-danger text-center" colspan="7">No records found.</td>
-        </tr>
-        @endforelse
+            @endforelse
         <tr>
             <th colspan="3">Grand Total</th>
             <td class="text-right">{{$summary_reports["grand_total_gross"]}}</td>
@@ -59,5 +60,10 @@
             <td class="text-right">{{$summary_reports["grand_total_rate"]}}</td>
             <td class="text-right">{{$summary_reports["grand_total_amount"]}}</td>
         </tr>
+        @else
+            <tr>
+                <td class="text-danger text-center" colspan="10">No records found for summary.</td>
+            </tr>
+        @endif
     </tbody>
 </table>
