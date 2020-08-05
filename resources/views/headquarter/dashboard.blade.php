93c4cfe6165721996616319f46c269b5abd565b0
Sunil Thatal <sunilthatal007@gmail.com>
2020-08-05 16:50:08 +0530

    minor fixed.

--- resources/views/headquarter/dashboard.blade.php
+++ resources/views/headquarter/dashboard.blade.php
@@ -15,6 +15,9 @@ Dashboard
 
                 <div class="info-box-content">
                     <span class="info-box-text">Factories</span>
+                    @php
+                        $guard = "web";
+                    @endphp
                     <span class="info-box-number">{{
                         \App\Models\Factory::whereIn("factory_id", function ($query) use ($guard) {
                             return $query->select("user_id")->from("factory_information")->where("headquarter_id", auth($guard)->id());
